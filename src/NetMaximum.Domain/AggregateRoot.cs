36fe4ba353e1b1cbd1ec75cdd1b40ef1915b4ec3
TheRubble <therubble@hotmail.com>
2022-01-24 15:34:58 +0000

    Changed the setter.

--- src/NetMaximum.Domain/AggregateRoot.cs
+++ src/NetMaximum.Domain/AggregateRoot.cs
@@ -2,7 +2,7 @@ namespace NetMaximum.Domain;
 
 public abstract class AggregateRoot
 {
-    public Guid Id { get; private set; }
+    public Guid Id { get; protected set; }
     
     protected AggregateRoot(Guid aggregateId)
     {
